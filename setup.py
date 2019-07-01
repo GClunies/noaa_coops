271ad4a46677129cb3285c37988de8b98bee9540
Greg Clunies <greg.clunies@gmail.com>
2019-06-30 19:10:06 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(name='noaa_coops',
       version='0.1',
-      description='Python wrapper to fetch NOAA Tides & Currents Data',
+      description='Python wrapper for NOAA Tides & Currents Data and Metadata',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/GClunies/noaa_coops',
