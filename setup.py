b3e94eac1a7880b62ad6df43b3e4afe0863e4e5d
GClunies <greg.clunies@gmail.com>
2020-08-08 19:30:53 -0700

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="noaa_coops",
-    version="0.1.6",
+    version="0.1.7",
     description="Python wrapper for NOAA Tides & Currents Data and Metadata",
     long_description=long_description,
     long_description_content_type="text/markdown",
