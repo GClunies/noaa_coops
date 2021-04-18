fae867ef4f92b34296ea0b60df5abe7271e45235
GClunies <greg.clunies@gmail.com>
2021-04-17 18:33:42 -0700

    Add currents example

---
+++ examples/currents_example.py
@@ -0,0 +1,13 @@
+import noaa_coops as nc
+
+oakland_outer_LB3 = nc.Station('s09010')
+currents = oakland_outer_LB3.get_data(
+    begin_date="20210414",
+    end_date="20210415",
+    product="currents",
+    bin_num=2,
+    units="metric",
+    time_zone="gmt",
+)
+
+currents.head(20)
