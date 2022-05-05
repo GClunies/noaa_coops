132def664b3ee39015f6a0f19c79d5a760c2906c
Kim Pevey <kcpevey@gmail.com>
2022-05-05 11:00:08 -0500

    linting

--- noaa_coops/noaa_coops.py
+++ noaa_coops/noaa_coops.py
@@ -47,13 +47,17 @@ class Station:
         ends = [x["last"] for x in response]
 
         unique_names = []
-        for name in names: 
-            if name not in unique_names: unique_names.append(name)
-        
+        for name in names:
+            if name not in unique_names:
+                unique_names.append(name)
+
         inventory_dict = {}
         for name in unique_names:
             idxs = [i for i, x in enumerate(names) if x == name]
-            inventory_dict[name] = {"start_date": [starts[i] for i in idxs][0], "end_date": [ends[i] for i in idxs][-1]}
+            inventory_dict[name] = {
+                "start_date": [starts[i] for i in idxs][0],
+                "end_date": [ends[i] for i in idxs][-1],
+            }
 
         self.data_inventory = inventory_dict
 
