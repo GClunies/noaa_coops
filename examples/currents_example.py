0427e6985a227f4a3476b6c5eec5b179ed019854
Greg Clunies <greg.clunies@gmail.com>
2023-02-05 11:24:01 -0800

    use poetry for CI

--- examples/currents_example.py
+++ examples/currents_example.py
@@ -1,6 +1,7 @@
 import noaa_coops as nc
 
-oakland_outer_LB3 = nc.Station('s09010')
+
+oakland_outer_LB3 = nc.Station("s09010")
 currents = oakland_outer_LB3.get_data(
     begin_date="20210414",
     end_date="20210415",
